c497843535f61c7a43efed6112c12b5ecdc62add
kuntydev <54059328+Ryanprw@users.noreply.github.com>
2025-03-16 00:13:14 +0700

    implementing carousel slider function

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,6 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:portal_news/model/category_model.dart';
+import 'package:portal_news/model/slider_model.dart';
 import 'package:portal_news/service/data.dart';
+import 'package:portal_news/service/slider_data.dart';
 
 class HomePages extends StatefulWidget {
   const HomePages({super.key});
@@ -11,9 +14,11 @@ class HomePages extends StatefulWidget {
 
 class _HomePagesState extends State<HomePages> {
   List<CategoryModel> categories = [];
+  List<sliderModel> sliders = [];
   @override
   void initState() {
     categories = getCategories();
+    sliders = getSliders();
     super.initState();
   }
 
@@ -52,11 +57,64 @@ class _HomePagesState extends State<HomePages> {
                 },
               ),
             ),
+            SizedBox(height: 30),
+            CarouselSlider.builder(
+              itemCount: sliders.length,
+              itemBuilder: (context, index, realIndex) {
+                String? res = sliders[index].image;
+                String? res1 = sliders[index].name;
+                return buildImage(res!, index, res1!);
+              },
+              options: CarouselOptions(
+                height: 250,
+                // viewportFraction: 1,
+                autoPlay: true,
+                enlargeCenterPage: true,
+                enlargeStrategy: CenterPageEnlargeStrategy.height,
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
+  Widget buildImage(String image, int index, String name) => Container(
+    margin: EdgeInsets.symmetric(horizontal: 5),
+    child: Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            image,
+            height: 250,
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 10),
+          margin: EdgeInsets.only(top: 170),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          child: Text(
+            name,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 class CategoryTitle extends StatelessWidget {
