703ee13f25f2e5ad3027340b042ef96f85240616
kuntydev <54059328+Ryanprw@users.noreply.github.com>
2025-03-17 01:23:43 +0700

    add new theme

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portal_news/service/app_constants.dart';
+
+class AppTheme {
+  static ThemeData get darkTheme {
+    return ThemeData.dark().copyWith(
+      primaryColor: AppConstants.primaryColor,
+      scaffoldBackgroundColor: AppConstants.backgroundColor,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppConstants.backgroundColor,
+        elevation: 0,
+      ),
+      textTheme: GoogleFonts.urbanistTextTheme(ThemeData.dark().textTheme),
+      cardTheme: CardTheme(
+        color: AppConstants.cardBackgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppConstants.cardBorderRadius),
+        ),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: AppConstants.backgroundColor,
+        selectedItemColor: AppConstants.primaryColor,
+        unselectedItemColor: Colors.white54,
+        type: BottomNavigationBarType.fixed,
+      ),
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        secondary: AppConstants.primaryColor,
+      ),
+    );
+  }
+}
